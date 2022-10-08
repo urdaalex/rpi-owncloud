28870aa49a1c8f9a3eeecf912aeb80f9e5514eca
Alex Urda <7596456+urdaalex@users.noreply.github.com>
2022-10-07 20:14:33 -0400

    added logging

--- crontab/syncdirs.sh
+++ crontab/syncdirs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rsync -aq --delete $SRC_DIR_PATH $DST_DIR_PATH
+if [ -f "$CRON_ERR_LOGS" ]; then rm "$CRON_ERR_LOGS"; fi
+
+rsync -aq --delete --log-file="$CRON_ERR_LOGS" "$SRC_DIR_PATH" "$DST_DIR_PATH"
