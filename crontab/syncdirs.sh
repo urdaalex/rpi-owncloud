80dbfaf37402a5d1520b7b0b591f7b6d3e009b34
Alex Urda <7596456+urdaalex@users.noreply.github.com>
2022-10-07 20:25:33 -0400

    fix

--- crontab/syncdirs.sh
+++ crontab/syncdirs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-if [ -f "$CRON_ERR_LOGS" ]; then rm "$CRON_ERR_LOGS"; fi
-
-rsync -aq --delete --log-file="$CRON_ERR_LOGS" "$SRC_DIR_PATH" "$DST_DIR_PATH"
+rsync -aq --delete "$SRC_DIR_PATH" "$DST_DIR_PATH" 2>"$CRON_ERR_LOGS"
